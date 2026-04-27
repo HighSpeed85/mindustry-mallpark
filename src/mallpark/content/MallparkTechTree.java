e526b2d31737179b237acb7c01e03f9584aa4a27
HighSpeed85 <HS85@mail.com>
2026-04-27 15:22:29 +1200

    Update MallparkTechTree.java

--- src/mallpark/content/MallparkTechTree.java
+++ src/mallpark/content/MallparkTechTree.java
@@ -14,6 +14,7 @@ import static mallpark.content.MallparkBlocks.*;
 public class MallparkTechTree {
 	static TechNode context = null;
 	
+	@Override
 	public static void load(){
 		node(copperWallLarge, () -> {
 			node(copperWallHuge, () -> {
