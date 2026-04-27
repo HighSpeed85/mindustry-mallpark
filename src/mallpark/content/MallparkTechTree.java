6a9941b3774a4d971f4ddedbed690cdbddef96b4
HighSpeed85 <HS85@mail.com>
2026-04-27 17:46:58 +1200

    Update MallparkTechTree.java

--- src/mallpark/content/MallparkTechTree.java
+++ src/mallpark/content/MallparkTechTree.java
@@ -34,11 +34,49 @@ public class MallparkTechTree {
 		});
 	}
 	
-	// ripped from BetaMindy until i find a more 'vanilla' solution
+	// stuff ripped from BetaMindy until i find a more 'vanilla' solution
 	private static void vanillaNode(UnlockableContent parent, Runnable children){
         context = TechTree.all.find(t -> t.content == parent);
         if(context != null){
             children.run();
         }
+    }
+	
+	private static void node(UnlockableContent content, ItemStack[] requirements, Seq<Objective> objectives, Runnable children){
+        TechNode node = new TechNode(context, content, requirements);
+        if(objectives != null) node.objectives = objectives;
+
+        TechNode prev = context;
+        context = node;
+        children.run();
+        context = prev;
+    }
+
+    private static void node(UnlockableContent content, ItemStack[] requirements, Runnable children){
+        node(content, requirements, null, children);
+    }
+
+    private static void node(UnlockableContent content, Seq<Objective> objectives, Runnable children){
+        node(content, content.researchRequirements(), objectives, children);
+    }
+
+    private static void node(UnlockableContent content, Runnable children){
+        node(content, content.researchRequirements(), children);
+    }
+
+    private static void node(UnlockableContent block){
+        node(block, () -> {});
+    }
+
+    private static void nodeProduce(UnlockableContent content, Seq<Objective> objectives, Runnable children){
+        node(content, content.researchRequirements(), objectives.add(new Produce(content)), children);
+    }
+
+    private static void nodeProduce(UnlockableContent content, Runnable children){
+        nodeProduce(content, Seq.with(), children);
+    }
+
+    private static void nodeProduce(UnlockableContent content){
+        nodeProduce(content, Seq.with(), () -> {});
     }
 }
